994724b96b1f201a72aaf0b609bb8d4cb28245fb
Collin Thommasen <=>
2022-04-02 15:00:36 -0700

    Added the layout for the past beers

--- frontend/src/components/beers/beers.js
+++ frontend/src/components/beers/beers.js
@@ -2,6 +2,20 @@ import React    from "react";
 import template from "./beers.jsx";
 
 class beers extends React.Component {
+  constructor(props) {
+    super(props);
+
+    let m1 = { index: 0, 
+               meetup: 'November 1, 2021 - April 22, 2022',
+               beers: [{brewery: 'Philips', beer: 'Blue Buck'}, {brewery: 'Field House', beer: 'Smoked Porter'}] 
+              };
+    let m2 = { index: 1,
+               meetup: 'Auguest 1, 2021 - November 1, 2021',
+               beers: [{brewery: 'Moon Underwater', beer: 'Coffee Sour' }, { brewery: 'Labatts', beer: 'Lucky'}]
+              };
+    const beers = [m1, m2];
+    this.state = { beers: beers };
+  }
   render() {
     return template.call(this);
   }
--- frontend/src/components/beers/beers.jsx
+++ frontend/src/components/beers/beers.jsx
@@ -1,11 +1,34 @@
 import "./beers.css";
 import React from "react";
+import Container from 'react-bootstrap/Container';
+import Row from 'react-bootstrap/Row';
+import Col from 'react-bootstrap/Col';
+import Alert from 'react-bootstrap/Alert';
+import Accordion from 'react-bootstrap/Accordion';
+import MeetupBeers from "./meetup-beers/meetup-beers";
 
 function template() {
   return (
-    <div className="beers">
-      <h1>beers</h1>
-    </div>
+    <Container className="beers">
+      <Row className="justify-content-md-center">
+        <Col xs md="6">
+          <Alert variant="primary">
+            <Alert.Heading>Past Beers</Alert.Heading>
+          </Alert>
+        </Col>
+      </Row>
+
+      <Row className="justify-content-md-center">
+        <Col xs md="6">
+          <Accordion>
+            {/* Render all the meetups that come from the backend */}
+            {
+              this.state.beers.map(el => <MeetupBeers meetup={el}></MeetupBeers>)
+            }
+          </Accordion>
+        </Col>
+      </Row>
+    </Container>
   );
 };
 
